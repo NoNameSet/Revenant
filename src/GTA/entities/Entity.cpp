9379b8c5bab4700a79981c4f6480b7e7f5ac4ec2
nonameset <git@elliotadams.de>
2024-04-26 11:46:08 +0200

    Added ENTITY:: natives

--- src/GTA/entities/Entity.cpp
+++ src/GTA/entities/Entity.cpp
@@ -43,12 +43,270 @@ namespace revenant
         return this->Exists() && this->handle != 0;
     }
 
+    bool Entity::HasBeenDamagedByAnyObject() const
+    {
+        return ENTITY::HAS_ENTITY_BEEN_DAMAGED_BY_ANY_OBJECT(this->handle);
+    }
+
+    bool Entity::HasBeenDamagedByAnyPed() const
+    {
+        return ENTITY::HAS_ENTITY_BEEN_DAMAGED_BY_ANY_PED(this->handle);
+    }
+
+    bool Entity::HasBeenDamagedByAnyVehicle() const
+    {
+        return ENTITY::HAS_ENTITY_BEEN_DAMAGED_BY_ANY_VEHICLE(this->handle);
+    }
+
+    bool Entity::HasBeenDamagedBy(const Entity &entity, bool includeVehicle) const
+    {
+        return ENTITY::HAS_ENTITY_BEEN_DAMAGED_BY_ENTITY(this->handle, entity.GetHandle(), includeVehicle);
+    }
+
+    bool Entity::CanSee(const Entity &entity, LOS_FLAGS flags) const
+    {
+        return ENTITY::HAS_ENTITY_CLEAR_LOS_TO_ENTITY(this->handle, entity.GetHandle(), static_cast<int>(flags));
+    }
+
+    bool Entity::CanSee(Entity &entity) const
+    {
+        return ENTITY::HAS_ENTITY_CLEAR_LOS_TO_ENTITY_IN_FRONT(this->handle, entity.GetHandle());
+    }
+
+    bool Entity::HasCollidedWithAnything() const
+    {
+        return ENTITY::HAS_ENTITY_COLLIDED_WITH_ANYTHING(this->handle);
+    }
+
+    bool Entity::IsPersistent() const
+    {
+        return ENTITY::IS_ENTITY_A_MISSION_ENTITY(this->handle);
+    }
+
+    bool Entity::IsAttached() const
+    {
+        return ENTITY::IS_ENTITY_ATTACHED(this->handle);
+    }
+
+    bool Entity::IsAttachedToAnyObject() const
+    {
+        return ENTITY::IS_ENTITY_ATTACHED_TO_ANY_OBJECT(this->handle);
+    }
+
+    bool Entity::IsAttachedToAnyPed() const
+    {
+        return ENTITY::IS_ENTITY_ATTACHED_TO_ANY_PED(this->handle);
+    }
+
+    bool Entity::IsAttachedToAnyVehicle() const
+    {
+        return ENTITY::IS_ENTITY_ATTACHED_TO_ANY_VEHICLE(this->handle);
+    }
+
+    bool Entity::IsAttachedTo(const Entity &entity) const
+    {
+        return ENTITY::IS_ENTITY_ATTACHED_TO_ENTITY(this->handle, entity.GetHandle());
+    }
+
+    bool Entity::IsAlive() const
+    {
+        return !ENTITY::IS_ENTITY_DEAD(this->handle, false);
+    }
+
+    bool Entity::IsDead() const
+    {
+        return ENTITY::IS_ENTITY_DEAD(this->handle, false);
+    }
+
+    bool Entity::IsInAir() const
+    {
+        return ENTITY::IS_ENTITY_IN_AIR(this->handle);
+    }
+
+    bool Entity::IsInWater() const
+    {
+        return ENTITY::IS_ENTITY_IN_WATER(this->handle);
+    }
+
+    bool Entity::IsOnScreen() const
+    {
+        return ENTITY::IS_ENTITY_ON_SCREEN(this->handle);
+    }
+
+    bool Entity::IsStatic() const
+    {
+        return ENTITY::IS_ENTITY_STATIC(this->handle);
+    }
+
+    bool Entity::IsTouching(const Entity &entity) const
+    {
+        return ENTITY::IS_ENTITY_TOUCHING_ENTITY(this->handle, entity.GetHandle());
+    }
+
+    bool Entity::IsUpright(float angle) const
+    {
+        return ENTITY::IS_ENTITY_UPRIGHT(this->handle, angle);
+    }
+
+    bool Entity::IsUpsideDown() const
+    {
+        return ENTITY::IS_ENTITY_UPSIDEDOWN(this->handle);
+    }
+
+    bool Entity::IsVisible() const
+    {
+        return ENTITY::IS_ENTITY_VISIBLE(this->handle);
+    }
+
+    bool Entity::IsOccluded() const
+    {
+        return ENTITY::IS_ENTITY_OCCLUDED(this->handle);
+    }
+
+    bool Entity::IsWaitingForCollision() const
+    {
+        return ENTITY::IS_ENTITY_WAITING_FOR_WORLD_COLLISION(this->handle);
+    }
+
+    bool Entity::IsCollisionEnabled() const
+    {
+        return !ENTITY::GET_ENTITY_COLLISION_DISABLED(this->handle);
+    }
+
+    float Entity::GetHeading() const
+    {
+        return ENTITY::GET_ENTITY_HEADING(this->handle);
+    }
+
+    float Entity::GetHeight() const
+    {
+        Vector3 position = GetPosition();
+        return ENTITY::GET_ENTITY_HEIGHT(this->handle, position.x, position.y, position.z, true, true);
+    }
+
+    float Entity::GetHeightAboveGround() const
+    {
+        return ENTITY::GET_ENTITY_HEIGHT_ABOVE_GROUND(this->handle);
+    }
+
+    float Entity::GetSpeed() const
+    {
+        return ENTITY::GET_ENTITY_SPEED(this->handle);
+    }
+
+    std::unique_ptr<Entity> Entity::GetEntityAttachedTo() const
+    {
+        return std::make_unique<Entity>(ENTITY::GET_ENTITY_ATTACHED_TO(this->handle));
+    }
+
+    Vector3 Entity::GetPosition() const
+    {
+        return ENTITY::GET_ENTITY_COORDS(this->handle, false);
+    }
+
+    Vector3 Entity::GetForwardVector() const
+    {
+        Vector3 forwardVector, rightVector, upVector, position;
+        ENTITY::GET_ENTITY_MATRIX(this->handle, &forwardVector, &rightVector, &upVector, &position);
+        return forwardVector;
+    }
+
+    Vector3 Entity::GetRearVector() const
+    {
+        Vector3 forwardVector, rightVector, upVector, position;
+        ENTITY::GET_ENTITY_MATRIX(this->handle, &forwardVector, &rightVector, &upVector, &position);
+        return { forwardVector.x, -forwardVector.y, forwardVector.z };
+    }
+
+    Vector3 Entity::GetRightVector() const
+    {
+        Vector3 forwardVector, rightVector, upVector, position;
+        ENTITY::GET_ENTITY_MATRIX(this->handle, &forwardVector, &rightVector, &upVector, &position);
+        return rightVector;
+    }
+
+    Vector3 Entity::GetLeftVector() const
+    {
+        Vector3 forwardVector, rightVector, upVector, position;
+        ENTITY::GET_ENTITY_MATRIX(this->handle, &forwardVector, &rightVector, &upVector, &position);
+        return { -rightVector.x, rightVector.y, rightVector.z };
+    }
+
+    Vector3 Entity::GetUpVector() const
+    {
+        Vector3 forwardVector, rightVector, upVector, position;
+        ENTITY::GET_ENTITY_MATRIX(this->handle, &forwardVector, &rightVector, &upVector, &position);
+        return upVector;
+    }
+
+    Vector3 Entity::GetBottomVector() const
+    {
+        Vector3 forwardVector, rightVector, upVector, position;
+        ENTITY::GET_ENTITY_MATRIX(this->handle, &forwardVector, &rightVector, &upVector, &position);
+        return { upVector.x, upVector.y, -upVector.z };
+    }
+
+    Vector3 Entity::GetPositionOffset(const Vector3 &position) const
+    {
+        return ENTITY::GET_OFFSET_FROM_ENTITY_GIVEN_WORLD_COORDS(this->handle, position.x, position.y, position.z);
+    }
+
+    Vector3 Entity::GetOffsetPosition(const Vector3 &offset) const
+    {
+        return ENTITY::GET_OFFSET_FROM_ENTITY_IN_WORLD_COORDS(this->handle, offset.x, offset.y, offset.z);
+    }
+
+    Vector3 Entity::GetRotation() const
+    {
+        return ENTITY::GET_ENTITY_ROTATION(this->handle, static_cast<int>(EULER_YXZ));
+    }
+
+    Vector3 Entity::GetVelocity() const
+    {
+        return ENTITY::GET_ENTITY_VELOCITY(this->handle);
+    }
+
+    Vector3 Entity::GetBoneRotation(int boneId) const
+    {
+        return ENTITY::GET_ENTITY_BONE_ROTATION(this->handle, boneId);
+    }
+
+    Vector3 Entity::GetBonePosition(int boneId) const
+    {
+        return ENTITY::GET_ENTITY_BONE_POSTION(this->handle, boneId);
+    }
+
     int Entity::GetHandle() const
     {
         return handle;
     }
 
-    std::unique_ptr<Model> Entity::GetModel()
+    int Entity::GetHealth() const
+    {
+        return ENTITY::GET_ENTITY_HEALTH(this->handle);
+    }
+
+    int Entity::GetMaxHealth() const
+    {
+        return ENTITY::GET_ENTITY_MAX_HEALTH(this->handle);
+    }
+
+    int Entity::GetBoneIndexByName(const std::string &boneName) const
+    {
+        return ENTITY::GET_ENTITY_BONE_INDEX_BY_NAME(this->handle, boneName.c_str());
+    }
+
+    int Entity::GetOpacity() const
+    {
+        return ENTITY::GET_ENTITY_ALPHA(this->handle);
+    }
+
+    ENTITY_TYPE Entity::GetType() const
+    {
+        return static_cast<ENTITY_TYPE>(ENTITY::GET_ENTITY_TYPE(this->handle));
+    }
+
+    std::unique_ptr<Model> Entity::GetModel() const
     {
         return std::make_unique<Model>(ENTITY::GET_ENTITY_MODEL(this->handle));
     }
@@ -56,5 +314,95 @@ namespace revenant
     uintptr_t Entity::GetMemoryAddress() const
     {
         return fwScriptGuid::GetBaseFromGuid(this->handle);
+    }
+
+    void Entity::SetMaxHealth(int value) const
+    {
+        ENTITY::SET_ENTITY_MAX_HEALTH(this->handle, value);
+    }
+
+    void Entity::SetHealth(int value) const
+    {
+        ENTITY::SET_ENTITY_HEALTH(this->handle, value, NULL, NULL);
+    }
+
+    void Entity::AttachTo(const Entity &entity, int boneId, const Vector3 &offset, const Vector3 &rotation, bool detachWhenDead, bool detachWhenRagdoll, bool activeCollisions, bool useBasicAttachIfPed, EULER_ROT_ORDER rotationOrder, bool attachOffsetIsRelative, bool markAsNoLongerNeededWhenDetached) const
+    {
+        ENTITY::ATTACH_ENTITY_TO_ENTITY(this->handle, entity.GetHandle(), boneId, offset.x, offset.y, offset.z, rotation.x, rotation.y, rotation.z, detachWhenDead, detachWhenRagdoll, activeCollisions, useBasicAttachIfPed, static_cast<int>(rotationOrder), attachOffsetIsRelative, markAsNoLongerNeededWhenDetached);
+    }
+
+    void Entity::Detach(bool applyVelocity, bool noCollisionUntilClear) const
+    {
+        ENTITY::DETACH_ENTITY(this->handle, applyVelocity, noCollisionUntilClear);
+    }
+
+    void Entity::FreezePosition(bool toggle) const
+    {
+        ENTITY::FREEZE_ENTITY_POSITION(this->handle, toggle);
+    }
+
+    void Entity::SetShouldFreezeWaitingOnCollision(bool toggle) const
+    {
+        ENTITY::SET_ENTITY_SHOULD_FREEZE_WAITING_ON_COLLISION(this->handle, toggle);
+    }
+
+    void Entity::MarkAsNoLongerNeeded()
+    {
+        ENTITY::SET_ENTITY_AS_NO_LONGER_NEEDED(&this->handle);
+    }
+
+    void Entity::SetCollision(bool toggle) const
+    {
+        ENTITY::SET_ENTITY_COLLISION(this->handle, toggle, false);
+    }
+
+    void Entity::SetPosition(const Vector3 &position) const
+    {
+        ENTITY::SET_ENTITY_COORDS(this->handle, position.x, position.y, position.z, false, true, true, false);
+    }
+
+    void Entity::SetDynamic(bool toggle) const
+    {
+        ENTITY::SET_ENTITY_DYNAMIC(this->handle, toggle);
+    }
+
+    void Entity::SetHeading(float value) const
+    {
+        ENTITY::SET_ENTITY_HEADING(this->handle, value);
+    }
+
+    void Entity::SetInvincible(bool toggle) const
+    {
+        ENTITY::SET_ENTITY_INVINCIBLE(this->handle, toggle);
+    }
+
+    void Entity::SetMaxSpeed(float value) const
+    {
+        ENTITY::SET_ENTITY_MAX_SPEED(this->handle, value);
+    }
+
+    void Entity::SetRotation(const Vector3 &rotation) const
+    {
+        ENTITY::SET_ENTITY_ROTATION(this->handle, rotation.x, rotation.y, rotation.z, static_cast<int>(EULER_YXZ), false);
+    }
+
+    void Entity::SetVisible(bool toggle) const
+    {
+        ENTITY::SET_ENTITY_VISIBLE(this->handle, toggle, false);
+    }
+
+    void Entity::SetVelocity(const Vector3 &value) const
+    {
+        ENTITY::SET_ENTITY_VELOCITY(this->handle, value.x, value.y, value.z);
+    }
+
+    void Entity::SetOpacity(int value) const
+    {
+        ENTITY::SET_ENTITY_ALPHA(this->handle, value, true);
+    }
+
+    void Entity::SetCollisionIgnoredEntity(const Entity &entity) const
+    {
+        ENTITY::SET_ENTITY_NO_COLLISION_ENTITY(this->handle, entity.GetHandle(), false);
     }
 }
--- src/GTA/entities/Entity.h
+++ src/GTA/entities/Entity.h
@@ -22,12 +22,84 @@ namespace revenant
 
         [[nodiscard]] bool Exists() const;
         [[nodiscard]] bool IsValid() const;
+        [[nodiscard]] bool HasBeenDamagedByAnyObject() const;
+        [[nodiscard]] bool HasBeenDamagedByAnyPed() const;
+        [[nodiscard]] bool HasBeenDamagedByAnyVehicle() const;
+        [[nodiscard]] bool HasBeenDamagedBy(const Entity& entity, bool includeVehicle) const;
+        [[nodiscard]] bool CanSee(const Entity& entity, LOS_FLAGS flags) const;
+        [[nodiscard]] bool CanSee(Entity& entity) const;
+        [[nodiscard]] bool HasCollidedWithAnything() const;
+        [[nodiscard]] bool IsPersistent() const;
+        [[nodiscard]] bool IsAttached() const;
+        [[nodiscard]] bool IsAttachedToAnyObject() const;
+        [[nodiscard]] bool IsAttachedToAnyPed() const;
+        [[nodiscard]] bool IsAttachedToAnyVehicle() const;
+        [[nodiscard]] bool IsAttachedTo(const Entity& entity) const;
+        [[nodiscard]] bool IsAlive() const;
+        [[nodiscard]] bool IsDead() const;
+        [[nodiscard]] bool IsInAir() const;
+        [[nodiscard]] bool IsInWater() const;
+        [[nodiscard]] bool IsOnScreen() const;
+        [[nodiscard]] bool IsStatic() const;
+        [[nodiscard]] bool IsTouching(const Entity& entity) const;
+        [[nodiscard]] bool IsUpright(float angle) const;
+        [[nodiscard]] bool IsUpsideDown() const;
+        [[nodiscard]] bool IsVisible() const;
+        [[nodiscard]] bool IsOccluded() const;
+        [[nodiscard]] bool IsWaitingForCollision() const;
+        [[nodiscard]] bool IsCollisionEnabled() const;
+
+        [[nodiscard]] float GetHeading() const;
+        [[nodiscard]] float GetHeight() const;
+        [[nodiscard]] float GetHeightAboveGround() const;
+        [[nodiscard]] float GetSpeed() const;
+
+        [[nodiscard]] std::unique_ptr<Entity> GetEntityAttachedTo() const;
+
+        [[nodiscard]] Vector3 GetPosition() const;
+        [[nodiscard]] Vector3 GetForwardVector() const;
+        [[nodiscard]] Vector3 GetRearVector() const;
+        [[nodiscard]] Vector3 GetRightVector() const;
+        [[nodiscard]] Vector3 GetLeftVector() const;
+        [[nodiscard]] Vector3 GetUpVector() const;
+        [[nodiscard]] Vector3 GetBottomVector() const;
+        [[nodiscard]] Vector3 GetPositionOffset(const Vector3& position) const;
+        [[nodiscard]] Vector3 GetOffsetPosition(const Vector3& offset) const;
+        [[nodiscard]] Vector3 GetRotation() const;
+        [[nodiscard]] Vector3 GetVelocity() const;
+        [[nodiscard]] Vector3 GetBoneRotation(int boneId) const;
+        [[nodiscard]] Vector3 GetBonePosition(int boneId) const;
 
         [[nodiscard]] int GetHandle() const;
+        [[nodiscard]] int GetHealth() const;
+        [[nodiscard]] int GetMaxHealth() const;
+        [[nodiscard]] int GetBoneIndexByName(const std::string& boneName) const;
+        [[nodiscard]] int GetOpacity() const;
 
-        std::unique_ptr<Model> GetModel();
+        [[nodiscard]] ENTITY_TYPE GetType() const;
+
+        [[nodiscard]] std::unique_ptr<Model> GetModel() const;
 
         [[nodiscard]] uintptr_t GetMemoryAddress() const;
+
+        void SetMaxHealth(int value) const;
+        void SetHealth(int value) const;
+        void AttachTo(const Entity& entity, int boneId, const Vector3& offset, const Vector3& rotation, bool detachWhenDead = false, bool detachWhenRagdoll = false, bool activeCollisions = false, bool useBasicAttachIfPed = false, EULER_ROT_ORDER rotationOrder = EULER_YXZ, bool attachOffsetIsRelative = true, bool markAsNoLongerNeededWhenDetached = false) const;
+        void Detach(bool applyVelocity = true, bool noCollisionUntilClear = true) const;
+        void FreezePosition(bool toggle) const;
+        void SetShouldFreezeWaitingOnCollision(bool toggle) const;
+        void MarkAsNoLongerNeeded();
+        void SetCollision(bool toggle) const;
+        void SetPosition(const Vector3& position) const;
+        void SetDynamic(bool toggle) const;
+        void SetHeading(float value) const;
+        void SetInvincible(bool toggle) const;
+        void SetMaxSpeed(float value) const;
+        void SetRotation(const Vector3& rotation) const;
+        void SetVisible(bool toggle) const;
+        void SetVelocity(const Vector3& value) const;
+        void SetOpacity(int value) const;
+        void SetCollisionIgnoredEntity(const Entity& entity) const;
     protected:
         int32_t handle;
     };
