500eaf79712205b899b94042ee3a9e2ad909c223
nonameset <git@elliotadams.de>
2024-04-27 17:33:38 +0200

    Added more PED:: natives

--- src/GTA/entities/peds/Ped.cpp
+++ src/GTA/entities/peds/Ped.cpp
@@ -72,24 +72,211 @@ namespace revenant
         TASK::TASK_WARP_PED_INTO_VEHICLE(this->handle, vehicle.GetHandle(), static_cast<int>(seat));
     }
 
-    bool Ped::IsStill()
+    void Ped::SetAccuracy(int value)
+    {
+        PED::SET_PED_ACCURACY(this->handle, value);
+    }
+
+    void Ped::SetArmor(int value)
+    {
+        PED::SET_PED_ARMOUR(this->handle, value);
+    }
+
+    void Ped::ResetLastVehicle()
+    {
+        PED::RESET_PED_LAST_VEHICLE(this->handle);
+    }
+
+    void Ped::SetMoney(int value)
+    {
+        PED::SET_PED_MONEY(this->handle, value);
+    }
+
+    void Ped::ClearLastDamageBone()
+    {
+        PED::CLEAR_PED_LAST_DAMAGE_BONE(this->handle);
+    }
+
+    void Ped::SetIdentificationRange(float value)
+    {
+        PED::SET_PED_ID_RANGE(this->handle, value);
+    }
+
+    int Ped::GetAccuracy() const
+    {
+        return PED::GET_PED_ACCURACY(this->handle);
+    }
+
+    int Ped::GetMoney() const
+    {
+        return PED::GET_PED_MONEY(this->handle);
+    }
+
+    int Ped::GetArmor() const
+    {
+        return PED::GET_PED_ARMOUR(this->handle);
+    }
+
+    int Ped::GetTimeOfDeath() const
+    {
+        return PED::GET_PED_TIME_OF_DEATH(this->handle);
+    }
+
+    uint32_t Ped::GetCauseOfDeath() const
+    {
+        return PED::GET_PED_CAUSE_OF_DEATH(this->handle);
+    }
+
+    PED_BONETAG Ped::GetLastDamageBone() const
+    {
+        int boneId;
+        PED::GET_PED_LAST_DAMAGE_BONE(this->handle, &boneId);
+        return static_cast<PED_BONETAG>(boneId);
+    }
+
+    bool Ped::IsStill() const
     {
         return TASK::IS_PED_STILL(this->handle);
     }
 
-    bool Ped::IsWalking()
+    bool Ped::IsWalking() const
     {
         return TASK::IS_PED_WALKING(this->handle);
     }
 
-    bool Ped::IsRunning()
+    bool Ped::IsRunning() const
     {
         return TASK::IS_PED_RUNNING(this->handle);
     }
 
-    bool Ped::IsSprinting()
+    bool Ped::IsSprinting() const
     {
         return TASK::IS_PED_SPRINTING(this->handle);
+    }
+
+    bool Ped::IsInVehicle(const Vehicle &vehicle, bool considerEnteringAsInVehicle) const
+    {
+        return PED::IS_PED_IN_VEHICLE(this->handle, vehicle.GetHandle(), considerEnteringAsInVehicle);
+    }
+
+    bool Ped::IsInAnyVehicle(bool considerEnteringAsInVehicle) const
+    {
+        return PED::IS_PED_IN_ANY_VEHICLE(this->handle, considerEnteringAsInVehicle);
+    }
+
+    bool Ped::IsInjured() const
+    {
+        return PED::IS_PED_INJURED(this->handle);
+    }
+
+    bool Ped::IsFatallyInjured() const
+    {
+        return PED::IS_PED_FATALLY_INJURED(this->handle);
+    }
+
+    bool Ped::IsHurt() const
+    {
+        return PED::IS_PED_HURT(this->handle);
+    }
+
+    bool Ped::IsReloading() const
+    {
+        return PED::IS_PED_RELOADING(this->handle);
+    }
+
+    bool Ped::IsPlayer() const
+    {
+        return PED::IS_PED_A_PLAYER(this->handle);
+    }
+
+    bool Ped::IsFacing(const Ped &ped, float angle) const
+    {
+        return PED::IS_PED_FACING_PED(this->handle, ped.GetHandle(), angle);
+    }
+
+    bool Ped::IsInMeleeCombat() const
+    {
+        return PED::IS_PED_IN_MELEE_COMBAT(this->handle);
+    }
+
+    bool Ped::IsStopped() const
+    {
+        return PED::IS_PED_STOPPED(this->handle);
+    }
+
+    bool Ped::IsShooting() const
+    {
+        return PED::IS_PED_SHOOTING(this->handle);
+    }
+
+    bool Ped::IsMale() const
+    {
+        return PED::IS_PED_MALE(this->handle);
+    }
+
+    bool Ped::IsFemale() const
+    {
+        return !PED::IS_PED_MALE(this->handle);
+    }
+
+    bool Ped::IsHuman() const
+    {
+        return PED::IS_PED_HUMAN(this->handle);
+    }
+
+    bool Ped::IsOnVehicle() const
+    {
+        return PED::IS_PED_ON_VEHICLE(this->handle);
+    }
+
+    bool Ped::IsSittingInVehicle(const Vehicle &vehicle) const
+    {
+        return PED::IS_PED_SITTING_IN_VEHICLE(this->handle, vehicle.GetHandle());
+    }
+
+    bool Ped::IsSittingInAnyVehicle() const
+    {
+        return PED::IS_PED_SITTING_IN_ANY_VEHICLE(this->handle);
+    }
+
+    bool Ped::IsOnFoot() const
+    {
+        return PED::IS_PED_ON_FOOT(this->handle);
+    }
+
+    bool Ped::IsFalling() const
+    {
+        return PED::IS_PED_FALLING(this->handle);
+    }
+
+    bool Ped::IsJumping() const
+    {
+        return PED::IS_PED_JUMPING(this->handle);
+    }
+
+    bool Ped::IsClimbing() const
+    {
+        return PED::IS_PED_CLIMBING(this->handle);
+    }
+
+    bool Ped::IsVaulting() const
+    {
+        return PED::IS_PED_VAULTING(this->handle);
+    }
+
+    bool Ped::IsDiving() const
+    {
+        return PED::IS_PED_DIVING(this->handle);
+    }
+
+    bool Ped::IsFleeing() const
+    {
+        return PED::IS_PED_FLEEING(this->handle);
+    }
+
+    TaskInvoker &Ped::GetTaskInvoker()
+    {
+        return taskInvoker;
     }
 
     std::unique_ptr<PedGroup> Ped::GetGroup() const
@@ -97,8 +284,13 @@ namespace revenant
         return std::make_unique<PedGroup>(PED::GET_PED_GROUP_INDEX(this->handle));
     }
 
-    TaskInvoker &Ped::GetTaskInvoker()
+    std::unique_ptr<Entity> Ped::GetSourceOfDeath()
     {
-        return taskInvoker;
+        return std::make_unique<Entity>(PED::GET_PED_SOURCE_OF_DEATH(this->handle));
+    }
+
+    std::unique_ptr<Vehicle> Ped::GetCurrentVehicle(bool considerEnteringAsInVehicle) const
+    {
+        return std::make_unique<Vehicle>(PED::GET_VEHICLE_PED_IS_IN(this->handle, false));
     }
 }
--- src/GTA/entities/peds/Ped.h
+++ src/GTA/entities/peds/Ped.h
@@ -28,15 +28,56 @@ namespace revenant
         void SetPathPreferToAvoidWater(bool toggle);
         void SetPathAvoidFire(bool toggle);
         void WarpIntoVehicle(const Vehicle& vehicle, VEHICLE_SEAT seat);
+        void SetAccuracy(int value);
+        void SetArmor(int value);
+        void ResetLastVehicle();
+        void SetMoney(int value);
+        void ClearLastDamageBone();
+        void SetIdentificationRange(float value);
 
-        bool IsStill();
-        bool IsWalking();
-        bool IsRunning();
-        bool IsSprinting();
+        [[nodiscard]] int GetAccuracy() const;
+        [[nodiscard]] int GetMoney() const;
+        [[nodiscard]] int GetArmor() const;
+        [[nodiscard]] int GetTimeOfDeath() const;
 
-        [[nodiscard]] std::unique_ptr<PedGroup> GetGroup() const;
+        [[nodiscard]] uint32_t GetCauseOfDeath() const;
+
+        [[nodiscard]] PED_BONETAG GetLastDamageBone() const;
+
+        [[nodiscard]] bool IsStill() const;
+        [[nodiscard]] bool IsWalking() const;
+        [[nodiscard]] bool IsRunning() const;
+        [[nodiscard]] bool IsSprinting() const;
+        [[nodiscard]] bool IsInVehicle(const Vehicle& vehicle, bool considerEnteringAsInVehicle) const;
+        [[nodiscard]] bool IsInAnyVehicle(bool considerEnteringAsInVehicle) const;
+        [[nodiscard]] bool IsInjured() const;
+        [[nodiscard]] bool IsFatallyInjured() const;
+        [[nodiscard]] bool IsHurt() const;
+        [[nodiscard]] bool IsReloading() const;
+        [[nodiscard]] bool IsPlayer() const;
+        [[nodiscard]] bool IsFacing(const Ped& ped, float angle) const;
+        [[nodiscard]] bool IsInMeleeCombat() const;
+        [[nodiscard]] bool IsStopped() const;
+        [[nodiscard]] bool IsShooting() const;
+        [[nodiscard]] bool IsMale() const;
+        [[nodiscard]] bool IsFemale() const;
+        [[nodiscard]] bool IsHuman() const;
+        [[nodiscard]] bool IsOnVehicle() const;
+        [[nodiscard]] bool IsSittingInVehicle(const Vehicle& vehicle) const;
+        [[nodiscard]] bool IsSittingInAnyVehicle() const;
+        [[nodiscard]] bool IsOnFoot() const;
+        [[nodiscard]] bool IsFalling() const;
+        [[nodiscard]] bool IsJumping() const;
+        [[nodiscard]] bool IsClimbing() const;
+        [[nodiscard]] bool IsVaulting() const;
+        [[nodiscard]] bool IsDiving() const;
+        [[nodiscard]] bool IsFleeing() const;
 
         TaskInvoker& GetTaskInvoker();
+
+        [[nodiscard]] std::unique_ptr<PedGroup> GetGroup() const;
+        std::unique_ptr<Entity> GetSourceOfDeath();
+        [[nodiscard]] std::unique_ptr<Vehicle> GetCurrentVehicle(bool considerEnteringAsInVehicle) const;
     protected:
         TaskInvoker taskInvoker;
     };
