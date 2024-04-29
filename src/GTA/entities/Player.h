8fc146b4d79682a063bacb427d9f7aa506029932
nonameset <git@elliotadams.de>
2024-04-29 16:08:09 +0200

    Added PLAYER:: natives; Added more enums to enums.h; Updated types.h

--- src/GTA/entities/Player.h
+++ src/GTA/entities/Player.h
@@ -4,12 +4,30 @@
 #pragma once
 
 #include "../entities/peds/Ped.h"
+#include "Model.h"
+#include "../entities/peds/PedGroup.h"
 
 namespace revenant
 {
     class Player
     {
     public:
+        static void SetModel(const Model& model);
+        static void SetWantedLevel(int level);
+        static void SetMaxWantedLevel(int level);
+        static void SetControl(bool setControlOn, SET_PLAYER_CONTROL_FLAGS flags);
+
+        static int GetWantedLevel();
+        static int GetMaxWantedLevel();
+        static int GetId();
+
+        static bool IsPlaying();
+        static bool IsFreeAiming();
+        static bool IsFreeAimingAtEntity(const Entity& entity);
+        static bool IsControlOn();
+
+        static std::unique_ptr<PedGroup> GetGroup();
+        static std::unique_ptr<Entity> GetFreeAimingTarget();
         static std::unique_ptr<Ped> GetCharacter();
     };
 }
