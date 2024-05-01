6e480d54a4451127616f0c85c01a51a840b0eab2
nonameset <git@elliotadams.de>
2024-05-01 19:59:40 +0200

    Implementation of rage::atHashValue

--- lib/rage.h
+++ lib/rage.h
@@ -5,16 +5,60 @@
 
 namespace rage
 {
+    enum atHashStringNamespaces
+    {
+        HSNS_ATHASHSTRING,
+        HSNS_ATFINALHASHSTRING,
+        HSNS_ATLITERALHASHSTRING,
+        HSNS_DIAGSTRING,
+        HSNS_STATNAMESTRING,
+        HSNS_NUM_NAMESPACES,
+        HSNS_INVALID = 255,
+        HSNS_MAX_NAMESPACES = 256,
+    };
+
     struct atHashStringStats
     {
         uint32_t m_refdStringCount;
         uint32_t m_refdStringChars;
     };
 
-    enum atHashStringNamespaces
+    class atNamespacedHashStringBase
     {
-        HSNS_ATFINALHASHSTRING,
-        HSNS_ATLITERALHASHSTRING
+    public:
+        void SetHash(const uint32_t hash)					{ m_hash = hash; }
+        uint32_t GetHash() const								{ return m_hash; }
+        void Clear()									{ m_hash = 0; }
+        bool IsNull() const								{ return m_hash == 0; }
+        bool IsNotNull() const							{ return m_hash != 0; }
+
+    protected:
+        uint32_t 		m_hash;
+    };
+
+    template<atHashStringNamespaces Namespace>
+    class atNamespacedHashValue : public atNamespacedHashStringBase
+    {
+    public:
+    };
+
+    class atHashValue : public atNamespacedHashValue<HSNS_ATHASHSTRING>
+    {
+        typedef atNamespacedHashValue<HSNS_ATHASHSTRING> Base;
+    public:
+        atHashValue() : Base() {}
+
+        // Define operator== and operator!= for comparing with atHashValue
+        bool operator==(const atHashValue& other) const {
+            return m_hash == other.m_hash;
+        }
+
+        bool operator!=(const atHashValue& other) const {
+            return m_hash != other.m_hash;
+        }
+
+        static atHashValue Null() { return atHashValue(); }
+        static const char* TryGetString(const uint32_t hash) { return nullptr; }
     };
 
     class atHashString
