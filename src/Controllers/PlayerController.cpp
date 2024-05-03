1e9827a7e4d58f7dc5ae052c17d7d287d1e8a50c
nonameset <git@elliotadams.de>
2024-05-03 16:36:00 +0200

    Added logger info.

--- src/Controllers/PlayerController.cpp
+++ src/Controllers/PlayerController.cpp
@@ -13,12 +13,16 @@ namespace revenant
         m_Initialized = true;
         m_PlayerState = PST_Survivor;
         m_DefaultModel = ENTITY::GET_ENTITY_MODEL(PLAYER::PLAYER_PED_ID());
+
+        g_Logger->Info("[PlayerController] PlayerController has been initialized.");
     }
 
     void PlayerController::Destroy()
     {
         m_PlayerState = PST_Normal;
         PLAYER::SET_PLAYER_MODEL(PLAYER::PLAYER_ID(), m_DefaultModel.GetModelHash());
+
+        g_Logger->Info("[PlayerController] PlayerController has been cleaned up.");
     }
 
     void PlayerController::Tick()
@@ -27,6 +31,13 @@ namespace revenant
 
     void PlayerController::SetPlayerState(PLAYER_STATE_TYPE stateType)
     {
+        if (m_PlayerState == stateType)
+            return;
+
+        g_Logger->Info("Changing player state from %s to %s",
+                       m_PlayerState == PST_Survivor ? "PST_Survivor" : "PST_Normal",
+                       stateType == PST_Normal ? "PST_Normal" : "PST_Survivor");
+
         switch (stateType)
         {
             case PST_Normal:
