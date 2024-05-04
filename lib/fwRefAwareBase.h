f469654dab1a3c4eea0d9fe63d166a595220c490
nonameset <git@elliotadams.de>
2024-05-04 23:04:34 +0200

    added fwRefAwareBase.h classes to rage namespace

--- lib/fwRefAwareBase.h
+++ lib/fwRefAwareBase.h
@@ -7,28 +7,38 @@ public:
 };
 static_assert(sizeof(datBase) == 0x08);
 
-class fwKnownRefHolder
+namespace rage
 {
-public:
-    fwKnownRefHolder(void** ppReference, fwKnownRefHolder* pNext);
+    class fwKnownRefHolder
+    {
+    public:
+        fwKnownRefHolder(void** ppReference, fwKnownRefHolder* pNext);
 
-    void**					m_ppReference;
-    fwKnownRefHolder*		m_pNext;
-};
-static_assert(sizeof(fwKnownRefHolder) == 0x10);
+        void**					m_ppReference;
+        fwKnownRefHolder*		m_pNext;
+    };
+    static_assert(sizeof(fwKnownRefHolder) == 0x10);
 
-template<class _Base> class fwRefAwareBaseImpl : public _Base
-{
-protected:
-    mutable fwKnownRefHolder*	m_pKnownRefHolderHead;
-private:
-    fwRefAwareBaseImpl( const fwRefAwareBaseImpl & )				{}
-    fwRefAwareBaseImpl &operator=( const fwRefAwareBaseImpl & )		{ return *this; }
-};
+    template<class _Base> class fwRefAwareBaseImpl : public _Base
+    {
+    protected:
+        mutable fwKnownRefHolder*	m_pKnownRefHolderHead;
+    private:
+        fwRefAwareBaseImpl( const fwRefAwareBaseImpl & )				{}
+        fwRefAwareBaseImpl &operator=( const fwRefAwareBaseImpl & )		{ return *this; }
+    };
 
-class fwRefAwareBase : public fwRefAwareBaseImpl<datBase>
-{
-};
-static_assert(sizeof(fwRefAwareBase) == 0x10);
+    class fwRefAwareBase : public fwRefAwareBaseImpl<datBase>
+    {
+    };
+    static_assert(sizeof(fwRefAwareBase) == 0x10);
+
+    template <class T, class B = fwRefAwareBase>
+    class fwRegdRef
+    {
+    public:
+        T* m_p;
+    };
+}
 
 #endif //REVENANT_FWREFAWAREBASE_H
