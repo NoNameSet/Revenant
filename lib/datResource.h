d70037aefe769d44bf6b0e664d8612291846225f
nonameset <git@elliotadams.de>
2024-05-07 19:42:06 +0200

    cleanup

--- lib/datResource.h
+++ lib/datResource.h
@@ -5,16 +5,10 @@
 
 #include "sysAllocator.h"
 
-class datResource {
+class datResource
+{
 public:
-    datResource(void *containerLs,void *containerPpu,size_t containerSize)
-    {
-        m_Next = rage::tlsContext::get()->m_resource;
-        rage::tlsContext::get()->m_resource = this;
-        m_Fixup = (int)containerLs - (int)containerPpu;
-        m_ContainerEa = (uint32_t)containerPpu;
-        m_ContainerSize = containerSize;
-    }
+    datResource(void *containerLs,void *containerPpu,size_t containerSize);
 
     ~datResource()
     {
@@ -26,49 +20,57 @@ public:
         return (uint32_t)ptr - m_ContainerEa < m_ContainerSize;
     }
 
-    template <class T> void PointerFixupNonNull(T& ptr) const
+    template <class T>
+    void PointerFixupNonNull(T& ptr) const
     {
         ptr = (T)((char*)(ptr) + m_Fixup);
     }
 
-    template <class T> void PointerFixup(T& ptr) const
+    template <class T>
+    void PointerFixup(T& ptr) const
     {
-        ptr = (T)((char*)(ptr) + (ptr? m_Fixup : 0));
+        ptr = (T)((char*)(ptr) + (ptr ? m_Fixup : 0));
     }
 
-    template <class T> static void Fixup(T &ptr) {
+    template <class T>
+    static void Fixup(T &ptr) {
         datResource *curr = rage::tlsContext::get()->m_resource;
         if (ptr && curr)
             curr->PointerFixupNonNull(ptr);
     }
 
-    template <class T> static void Place(T &ptr)
+    template <class T>
+    static void Place(T &ptr)
     {
         datResource *curr = rage::tlsContext::get()->m_resource;
-        if (ptr && curr) {
+        if (ptr && curr)
+        {
             curr->PointerFixupNonNull(ptr);
-            ptr->Place(ptr,*curr);
+            ptr->Place(ptr, *curr);
         }
     }
 
-    template <class T1,class T2> static void Place(T1 &ptr, T2 p1)
+    template <class T1,class T2>
+    static void Place(T1 &ptr, T2 p1)
     {
         datResource *curr = rage::tlsContext::get()->m_resource;
-        if (ptr && curr) {
+        if (ptr && curr)
+        {
             curr->PointerFixupNonNull(ptr);
-            ptr->Place(ptr,*curr,p1);
+            ptr->Place(ptr, *curr, p1);
         }
     }
 
-    template <class T1,class T2,class T3> static void Place(T1 &ptr, T2 p1, T3 p2)
+    template <class T1,class T2,class T3>
+    static void Place(T1 &ptr, T2 p1, T3 p2)
     {
         datResource *curr = rage::tlsContext::get()->m_resource;
-        if (ptr && curr) {
+        if (ptr && curr)
+        {
             curr->PointerFixupNonNull(ptr);
-            ptr->Place(ptr,*curr,p1,p2);
+            ptr->Place(ptr, *curr, p1, p2);
         }
     }
-
 private:
     int m_Fixup;
     uint32_t m_ContainerEa, m_ContainerSize;
